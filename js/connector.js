a4e778770185289ceb19f20266dd8465bd31901a
ShanghaiQuintic <shanghaiquintic@gmail.com>
2017-12-17 19:23:39 +0100

    Update connector.js

--- js/connector.js
+++ js/connector.js
@@ -1,5 +1,5 @@
-var WHITE_ICON = './images/icon-white.svg';
-var GRAY_ICON = './images/icon-gray.svg';
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 
 console.log('line 4')
 window.TrelloPowerUp.initialize({
